5727f6d6ffe52c539ffe094720a68baa7beb83c4
Steven Tan <steven@steventan.com.au>
2018-07-16 17:44:57 +1000

    add bulma/axios support

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
+import axios from './api'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+import './assets/styles/main.scss'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  axios,
   render: h => h(App)
 }).$mount('#app')
